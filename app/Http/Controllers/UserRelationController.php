18748986bc92859aaff30b7f3cfe051500b05295
Your Name <you@example.com>
2024-05-03 22:35:01 +0900

    laravelのみで対応

--- app/Http/Controllers/UserRelationController.php
+++ app/Http/Controllers/UserRelationController.php
@@ -15,54 +15,46 @@ class UserRelationController extends Controller
     {
         $userId = 10; // 表示するユーザーIDを設定
     
-        // ユーザー10がフォローしている人たちのデータを取得
+        // ユーザーがフォローしている人たちのデータを取得し、加工
         $following = UserRelation::where('user_id', $userId)
-                        ->get(['target_user_id', 'is_following', 'is_blocking']);
-    
-        // ユーザー10をフォローしている人たちのデータを取得
+                        ->get(['target_user_id', 'is_following', 'is_blocking'])
+                        ->keyBy('target_user_id')
+                        ->map(function ($item) {
+                            return [
+                                'follow' => $item->is_following,
+                                'follower' => false,
+                                'mutual' => false,
+                                'blocking' => $item->is_blocking,
+                                'blocked_by' => false  // 初期状態では不明
+                            ];
+                        });
+
+        // ユーザーをフォローしている人たちのデータを取得し、加工
         $followers = UserRelation::where('target_user_id', $userId)
-                        ->get(['user_id', 'is_following', 'is_blocking']);
-    
-        $relations = [];
-        // ユーザーがフォローしている人たちに関するデータを処理するループ
-        foreach ($following as $follow) {
-            // 初期化と同時にユーザーがフォローしている人たちの関係を設定
-            $relations[$follow->target_user_id] = [
-                'follow' => $follow->is_following,  // ユーザーが他のユーザーをフォローしているかどうか
-                'follower' => false,  // この段階ではフォロワー情報は不明なのでfalse
-                'mutual' => false,  // 相互フォローの情報もこの段階では不明なのでfalse
-                'blocking' => $follow->is_blocking,  // ユーザーが他のユーザーをブロックしているかどうか
-                'blocked_by' => false  // この段階では誰にブロックされているかは不明なのでfalse
-            ];
-        }
+                        ->get(['user_id', 'is_following', 'is_blocking'])
+                        ->each(function ($item) use ($following) {
+                            // 既存のデータの更新、または新しいデータの追加
+                            $relation = $following->get($item->user_id, [
+                                'follow' => false,
+                                'follower' => $item->is_following && !$item->is_blocking,
+                                'mutual' => false,
+                                'blocking' => false,
+                                'blocked_by' => false
+                            ]);
 
-        // ユーザーをフォローしている人たちに関するデータを処理するループ
-        foreach ($followers as $follower) {
-            // フォロワー情報が$relationsにまだ設定されていない場合、初期設定を行う
-            if (!isset($relations[$follower->user_id])) {
-                $relations[$follower->user_id] = [
-                    'follow' => false,  // フォロー情報はこの段階では不明なのでfalse
-                    'follower' => false,  // 初期設定でfalseを指定（この後で真偽値が設定される）
-                    'mutual' => false,  // 相互フォロー情報はこの段階では不明なのでfalse
-                    'blocking' => false,  // ブロッキング情報はこの段階では不明なのでfalse
-                    'blocked_by' => false  // ブロックされている情報はこの段階では不明なのでfalse
-                ];
-            }
-            // フォロワーによるフォロワー情報設定
-            $relations[$follower->user_id]['follower'] = $follower->is_following && !$follower->is_blocking;
-             // $relations配列にフォロー情報があればそれを使い、なければfalseを設定
-            $relations[$follower->user_id]['follow'] = $relations[$follower->user_id]['follow'] ?? false;
-            // 互いにフォローし合っていれば相互フォローとしてtrueを設定
-            $relations[$follower->user_id]['mutual'] = $relations[$follower->user_id]['follow'] && $follower->is_following;
-            // フォロワーによるブロック情報を設定
-            $relations[$follower->user_id]['blocked_by'] = $follower->is_blocking && !$follower->is_following;
-        }
+                            $relation['follower'] = $item->is_following && !$item->is_blocking;
+                            $relation['mutual'] = $relation['follow'] && $item->is_following;
+                            $relation['blocked_by'] = $item->is_blocking && !$item->is_following;
 
-    
+                            $following->put($item->user_id, $relation);
+                        });
+
         // 配列をキー（ID）で昇順にソート
-        ksort($relations);
-    
-        return view('user_relations.index', compact('relations'));
+        $following = $following->sortKeys();
+
+        return view('user_relations.index', ['relations' => $following]);
+
+
     }
     
     
